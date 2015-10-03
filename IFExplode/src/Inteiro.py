53cd03fac932e16e3984a3503d9c68c77771e8bb
Ldipr3 <lucas.dipre@gmail.com>
2015-10-03 19:47:45 -0300

    Versão 1.2
    Fazendo a tela andar(as plataformas) quando o player anda e criação de
    uma "camera" que seque o player o o tempo todo, ou seja o player só se
    move dentro da tela, e quando ele chega em um certo ponto da tela a tela
    e todsas as plataformas tem sua posição atualizada e são todas
    redesenhadas

--- IFExplode/src/Inteiro.py
+++ IFExplode/src/Inteiro.py
@@ -22,7 +22,7 @@ from pygame.locals import *
 
 class Player (pygame.sprite.Sprite):
     # define as propriedades da imagem como cor e tamanho
-    def __init__(self, color = blue, largura = 28, altura = 44):
+    def __init__(self, color = blue, largura = 70, altura = 86):
         
         super( Player, self ).__init__() #pega as caracteristicas do pai e construtor
         self.image = pygame.Surface((largura, altura))
@@ -38,16 +38,10 @@ class Player (pygame.sprite.Sprite):
     def set_properties(self):
         
         self.rect = self.image.get_rect() #da propriedades de um retangulo a imagem feito pelao pygame
-        
         self.origin_x = self.rect.centerx
         self.origin_y = self.rect.centery
         
         self.speed = 5
-        
-    
-    def change_speed(self, hspeed, vspeed):
-        self.hspeed += hspeed
-        self.vspeed += vspeed
         
     
     #Define a posicao em que o objetos sera criado
@@ -55,11 +49,15 @@ class Player (pygame.sprite.Sprite):
         self.rect.x = x - self.origin_x
         self.rect.y = y - self.origin_y
     
+    def set_fase(self, fase):
+        self.fase = fase
+        self.set_position(fase.player_inicio_x, fase.player_inicio_y)
+    
     #Carrega a imagem na sprite passando o caminho aonde esta a imagem     
     def set_image(self, filename = None):
         if (filename != None):
             
-            self.image = pygame.image.load(filename)          
+            self.image = pygame.image.load(filename)         
             self.set_properties()
     
     def update(self, collidable = pygame.sprite.Group() , event = None): #atualiza a imagem detectando a colisao parando o objeto
@@ -79,7 +77,6 @@ class Player (pygame.sprite.Sprite):
                 self.rect.left = collided_object.rect.right
         
         self.rect.y += self.vspeed
-        
         collision_list = pygame.sprite.spritecollide(self, collidable, False ) #lista de objetos colidiveis
         
         for collided_object in collision_list:
@@ -95,27 +92,32 @@ class Player (pygame.sprite.Sprite):
         if not ( event == None ):
             if(event.type == pygame.KEYDOWN):
                 if (event.key == pygame.K_LEFT):
-                    self.change_speed( -(self.speed), 0 )
+                    self.hspeed = -self.speed
                 if (event.key == pygame.K_RIGHT):
-                    self.change_speed( (self.speed), 0 )
+                    self.hspeed = self.speed
                 if (event.key == pygame.K_UP):
-                    if(self.vspeed == 0):
-                        self.change_speed( 0, -(self.speed)*2.5) # dobra a velocidade e pula
+                    if(len(collision_list)>=1):
+                        self.vspeed = -(self.speed)*2.0 # dobra a velocidade e pula
+                '''
                 if (event.key == pygame.K_DOWN):
                     #self.change_speed( 0, 5 )
                     pass
+                '''
+            
             if(event.type == pygame.KEYUP):
                 if (event.key == pygame.K_LEFT):
-                    if (self.hspeed != 0): self.hspeed = 0
+                    if (self.hspeed < 0): self.hspeed = 0
                 if (event.key == pygame.K_RIGHT):
-                    if (self.hspeed != 0): self.hspeed = 0
+                    if (self.hspeed > 0): self.hspeed = 0
+                '''
+                comentado pois temos gravidade, pode ser usado depois
                 if (event.key == pygame.K_UP):
                     #if (self.vspeed != 0): self.vspeed = 0
                     pass
                 if (event.key == pygame.K_DOWN):
                     #if (self.vspeed != 0): self.vspeed = 0
                     pass
-                
+                '''
     def aplicar_gravidade(self, gravidade = .50):
         if (self.vspeed == 0): self.vspeed = 1
         else: self.vspeed += gravidade
@@ -143,6 +145,16 @@ class Fase(object): #utiliza o novo jeito do python de usar heranca colocando ob
         
         self.lista_de_objetos = pygame.sprite.Group() # grupo para os objeto das plataformas da fase
         self.objeto_player = objeto_player
+        self.player_inicio = self.player_inicio_x, self.player_inicio_y = 0,0 
+    
+        self.deslocamento_mundo_x = self.deslocamento_mundo_y = 0 #o quanto o mundo da fase muda
+        
+        self.visao_esquerda = window_largura/2 - window_largura/8
+        self.visao_direita = window_largura/2 - window_largura/10
+        
+        self.visao_cima = window_altura/5 
+        self.visao_baixo = window_altura/4 - window_altura/10
+             
         
     def update(self):
         self.lista_de_objetos.update() #atualiza todos os objetos do grupo de objetos
@@ -152,14 +164,46 @@ class Fase(object): #utiliza o novo jeito do python de usar heranca colocando ob
         window.fill(white) #desenha o fundo da janela
         
         self.lista_de_objetos.draw(window) #desenha os objetos da fase na janela
+    
+    def desloca_mundo(self, desloca_x, desloca_y):
         
+        self.deslocamento_mundo_x += desloca_x
+        self.deslocamento_mundo_y += desloca_y
+        
+        for cada_objeto in self.lista_de_objetos:
+            cada_objeto.rect.x += desloca_x
+            cada_objeto.rect.y += desloca_y
     
-    
+    def controla_camera(self):
+        
+        if(self.objeto_player.rect.x <= self.visao_esquerda):
+            diferenca_visao = self.visao_esquerda - self.objeto_player.rect.x #diferenca do quanto podemos ir menos o quanto ja fomos para aquele lado
+            self.objeto_player.rect.x = self.visao_esquerda
+            self.desloca_mundo(diferenca_visao, 0)
+            
+        if(self.objeto_player.rect.x >= self.visao_direita):
+            diferenca_visao = self.visao_direita - self.objeto_player.rect.x #diferenca do quanto podemos ir menos o quanto ja fomos para aquele lado
+            self.objeto_player.rect.x = self.visao_direita
+            self.desloca_mundo(diferenca_visao, 0)
+        
+        if(self.objeto_player.rect.y <= self.visao_cima):
+            diferenca_visao = self.visao_cima - self.objeto_player.rect.y #diferenca do quanto podemos ir menos o quanto ja fomos para aquele lado
+            self.objeto_player.rect.y = self.visao_cima
+            self.desloca_mundo(0, diferenca_visao)
+            
+        if(self.objeto_player.rect.y >= self.visao_baixo):
+            diferenca_visao = self.visao_baixo - self.objeto_player.rect.y #diferenca do quanto podemos ir menos o quanto ja fomos para aquele lado
+            self.objeto_player.rect.y = self.visao_baixo
+            self.desloca_mundo(0, diferenca_visao)
+            
 class Fase_1(Fase):
     
     def __init__(self, objeto_player):
         
         super (Fase_1, self).__init__(objeto_player)
+        
+        #define um lugas especifico aonde o player vai iniciar naquela fase
+        self.player_inicio = self.player_inicio_x, self.player_inicio_y = 100,0
         
         level = [
             # [ x, y, largura, altura, color ]
@@ -171,15 +215,14 @@ class Fase_1(Fase):
             block = Block(block[0], block[1], block[2], block[3], block[4])
             self.lista_de_objetos.add(block)
         
-
+'''
 def set_message( text ):
     global message, previous_message
     message = font.render( text, True, black, white )
     previous_message = message
-    
+'''
+
 pygame.init()
-
-
 
 # Definicoes da Janela
 window_tamanho = window_largura, window_altura = 1024, 768
@@ -201,15 +244,17 @@ lista_Fases.append(Fase_1( player )) #adiciona a fase 1 a lista e passa o objeto
 fase_atual_numero = 0 # um numero para identificar a fase atual
 fase_atual = lista_Fases[fase_atual_numero] #guarda a posicao da fase atual da lista
 
-player.fase = fase_atual
+player.set_fase(fase_atual)
 
 
 
 font = pygame.font.SysFont("Times New Roman, Arial", 30) # carrega a fonte 
 #text = font.render("IFExplode!!!", True, black) #renderiza a fonte
 
+'''
 massage = previous_message = None
 set_message("")        
+'''
 
 running = True # Variavel para o loop da janela principal
             
@@ -227,7 +272,9 @@ while running:
     fase_atual.update()
     
     
-    #Testes de logica
+    #Testes de logica do jogo
+    
+    fase_atual.controla_camera()
     
     #Desenha e redesenha tudo
     
